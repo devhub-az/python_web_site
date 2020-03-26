0552b563ace189aabfd8e084a24edb39db390235
Hose <hose1021@gmail.com>
2020-03-26 14:06:11 +0400

    Fix settings.py

--- PCW/settings.py
+++ PCW/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'PCW.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',  # set your db name
-        'USER': '',  # set your db user
-        'PASSWORD': '',  # set your password
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
