d402415b848275cac616c0b9468adf13a7174822
Hose <hose1021@gmail.com>
2020-04-11 14:42:59 +0400

    Edited Index page, Changed docker to dev mode.

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,6 +2,7 @@ window.axios = require('axios');
 window.Vue = require('vue');
 
 Vue.component('index', require('./components/index').default);
+Vue.component('pagination', require('./components/plugins/pagination').default);
 
 Vue.config.productionTip = false;
 Vue.config.silent = false;
