a3d9fbf29bf8cbf67ddc6758599c067d747ad9d2
Hose <hose1021@gmail.com>
2020-04-11 13:18:57 +0400

    Docker Added to docker postgress and edited example .env file

--- src/pcw/settings.py
+++ src/pcw/settings.py
@@ -83,13 +83,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     }
-# }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- src/pcw/urls.py
+++ src/pcw/urls.py
@@ -1,18 +1,3 @@
-"""PCW URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -22,8 +7,8 @@ from django.conf.urls import handler404, handler500
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
-    path('api/',include('api.urls')),
-    path('seminar/',include('seminars.urls')),
+    path('api/', include('api.urls')),
+    path('seminar/', include('seminars.urls')),
     path('', include('blog.urls')),
 ]
 
